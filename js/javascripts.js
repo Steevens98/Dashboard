b7faa4d6254e345c4de50b89695a5cfa9a699157
Steevens98 <67281150+Steevens98@users.noreply.github.com>
2023-06-14 14:47:47 -0500

    Add files via upload

--- js/javascripts.js
+++ js/javascripts.js
@@ -1,26 +1,77 @@
+let plot = (data) => { 
+  const ctx = document.getElementById('myChart');
+  const dataset = {
+    labels: data.hourly.time, /* ETIQUETA DE DATOS */
+    datasets: [{
+        label: 'Temperatura semanal', /* ETIQUETA DEL GRÁFICO */
+        data: data.hourly.temperature_2m, /* ARREGLO DE DATOS */
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+    }]
+  };
+  const config = {
+    type: 'line',
+    data: dataset,
+  };
+  const chart = new Chart(ctx, config)
+}
+
+let uv = (data) => { 
+  const ctx = document.getElementById('mybarChart');
+  const dataset = {
+    labels: data.daily.uv_index_max,/* ETIQUETA DE DATOS */
+    datasets: [{
+        label: 'Temperatura semanal', /* ETIQUETA DEL GRÁFICO */
+        data: data.daily.uv_index_max, /* ARREGLO DE DATOS */
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+    }]
+  };
+  const config = {
+    type: 'bar',
+    data: dataset,
+  };
+  const chart = new Chart(ctx, config)
+}
+
+let load = (data) => { 
+  console.log(data);
+  let timezone = data["timezone"]
+  let timezoneHTML = document.getElementById("Timezone")
+  timezoneHTML.textContent = timezone;
+  
+  let elevation = data["elevation"]
+  let elevationHTML = document.getElementById("elevacion")
+  elevationHTML.textContent = elevation;
+ 
+  let latitude= data["latitude"]
+  let latitudeHTML = document.getElementById("Latitud")
+  latitudeHTML.textContent = latitude;
+  
+  let longitude= data["longitude"]
+  let longitudeHTML = document.getElementById("Longitud")
+  longitudeHTML.textContent = longitude;
+  
+  plot(data)
+  uv(data)
+
+ }
+
 (function () {
+  let meteo = localStorage.getItem('meteo');
+  if(meteo == null) {
   let URL =
-    "https://api.open-meteo.com/v1/forecast?latitude=-2.14&longitude=-79.97&hourly=temperature_2m&timezone=auto";
+    "https://api.open-meteo.com/v1/forecast?latitude=-2.14&longitude=-79.97&hourly=temperature_2m&daily=uv_index_max&current_weather=true&timezone=auto";
   fetch(URL)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
-      let timezone = data["timezone"]
-      let timezoneHTML = document.getElementById("timezone0")
-      timezoneHTML.textContent = timezone;
-      
-      let elevation = data["elevation"]
-      let elevationHTML = document.getElementById("elevacion")
-      elevationHTML.textContent = elevation;
-     
-      let latitude= data["latitude"]
-      let latitudeHTML = document.getElementById("Latitud")
-      latitudeHTML.textContent = latitude;
-      
-      let longitude= data["longitude"]
-      let longitudeHTML = document.getElementById("Longitud")
-      longitudeHTML.textContent = longitude;
-      
+      load(data)
+      localStorage.setItem("meteo", JSON.stringify(data))
     })
     .catch(console.error);
+  }else{
+    load(JSON.parse(meteo))
+  }
 })();
